2fb7a44fd3168be44a6d82ab9ca91f1ceb800a90
Karan Samani <karan_samani@yahoo.co.in>
2021-06-22 23:04:00 +0100

    Version 1 release.
    Has time based brightness adjustment
    Has direct brightness adjustment to move to a specific setting based on
    config file

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# Requirements
-pip install pyyaml pip install pyyaml geocoder suntime
+# Project display_brightness
+
+Change the brightness of all your monitors in one go. Currently, this project only works on Windows (tested on Windows 10). Run the commands in command promt or powershell.
+
+## Setup of config.yaml
+
+Every entry under the `brightness_values` section represents an option for your monitors' brightness levels. Check Usage for more information on how to switch to a setting.
+
+Every entry under a setting represents the monitor and their respective brightness values [0-100]. Number 0 is reserved for the primary monitor. The rest mapped on a trial and error basis at the moment (change a value and see which monitor is changing brightness).
+
+```yaml
+brightness_values:
+  default:
+    0: 82
+    1: 100
+    2: 100
+  max:
+    0: 82
+    1: 100
+    2: 100
+  
+```
+
+## Requirements
+
+Install the following prerequisite python3 libraries
+```bash
+pip3 install screen_brightness_control pyyaml geocoder suntimes
+```
+
+## Usage
+
+Get help
+
+```bash
+python3 display_brightness.py -h
+```
+
+Running the script without any arguments sets the brightness to `default`.
+
+```bash
+python3 display_brightness.py
+```
+
+Running the script with `-l` or `--level` allows you to set the level of brightness from the options defined in the `config.yaml` file.
+
+```bash
+python3 display_brightness.py -l max
+```
+
+Running the script with the `-t` or `--time` flag set enables automatic time based brightness control. 
+
+Conditions are:
+
+|              |    |              | Brightness level |
+|--------------|----|--------------|------------------|
+| Current Time | <= | Sunrise Time |       `min`      |
+| Current Time | <= | Sunset Time  |       `max`      |
+| Current Time | >  | Sunset time  |       `min`      |
+
+
+allows you to set the level of brightness from the options defined in the `config.yaml` file.
+
+```bash
+python3 display_brightness.py -t
+```
--- display_brightness.py
+++ display_brightness.py
@@ -4,27 +4,34 @@ import screen_brightness_control as sbc
 
 import argparse, yaml, geocoder
 
-from suntime import Sun, SunTimeException
-from datetime import datetime
+from suntimes import SunTimes
+from datetime import datetime, timezone
 
 
 def get_times():
-    """get current time (ct), sunset (ss) time and sunrise (ss) time 
-    for the current lattiude and longitude"""
+    """
+    get current time (ct), sunset (ss) time and sunrise (ss) time 
+    for the current lattiude and longitude
+    """
     
     latitude, longitude = geocoder.ip('me').latlng
 
-    sun = Sun(latitude, longitude)
+    sun = SunTimes(latitude, longitude)
+    current_time = datetime.now(timezone.utc)
+    today_sr = sun.riselocal(current_time)
+    today_ss = sun.setlocal(current_time)
 
-    today_sr = sun.get_sunrise_time()
-    today_ss = sun.get_sunset_time()
-    current_time = datetime.now()
+    return (current_time, today_sr, today_ss)
 
-    return current_time, today_sr, today_ss
-
-def format_time(time, time_format='%H:%M'):
+def format_times(times, time_format='%H:%M'):
     """format time in the given string time_format"""
-    return format(time.strftime(time_format))
+    output = []
+    if type(times) in (type([]), type(())):
+        for time in times:
+            output.append(format(time.strftime(time_format)))
+    else:
+        output = format(times.strftime(time_format))
+    return output
 
 def parse_args(config):
     """parse passed in arguments"""
@@ -35,6 +42,11 @@ def parse_args(config):
         choices=list(config["brightness_values"].keys()),
         help='Set brightness level based on options from config.yaml file',
         required=False
+    )
+    parser.add_argument('--time', '-t',
+        help='Set brightness level based on sunset and sunrise times',
+        required=False,
+        action="store_true"
     )
     return parser.parse_args()
 
@@ -50,7 +62,8 @@ def load_config(location="config.yaml"):
     return config
 
 def set_brightness(config, displays, brightness_level):
-    """set the brightness for all the displays based on the config 
+    """
+    set the brightness for all the displays based on the config 
     and the brightness level
     """
     for display in displays:
@@ -61,14 +74,25 @@ def set_brightness(config, displays, brightness_level):
     return True
 
 def set_brightness_level(args, config):
-    """set the brightness level based on the argument(s) or other parameters"""
-    return args.level
+    """set the brightness level based on the argument(s) or other time"""
+    level = "default"
+    if args.level:
+        level = args.level
+
+    if args.time:    
+        ct, sr, ss = get_times()
+        if ct <= sr: # current time less than or equal sunrise time (no sun)
+            level = "min"
+        elif ct <= ss: # current time less than or equal to sunset time (sun)
+            level = "max"
+        else: # current time more than sunset time (no sun)
+            level = "min"
+
+    return level
 
 def main():
     config = load_config("config.yaml")
     args = parse_args(config)
-    ct, sr, ss = get_times()
-    print(get_times())
     brightness_level = set_brightness_level(args, config)
     displays = config["brightness_values"][brightness_level].keys()
     set_brightness(config, displays, brightness_level)
